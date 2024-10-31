5a02686809fd35cf477335f88e0da117b1f34cbd
Ritik <ritiklrt2@gmail.com>
2024-10-31 21:08:08 +0530

    compleate nextauth config

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,5 +1,5 @@
 import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/User";
+import UserModel from "@/model/User.model";
 import { sendVeficationEmail } from "@/helper/sendVerificationEmail";
 import bcrypt from "bcryptjs"
 
@@ -21,32 +21,60 @@ export async function POST(request: Request) {
             }, { status: 400 })
         };
         const existingUserByEmail = await UserModel.findOne({ email });
-        const otp = Math.floor(100000 + Math.random() * 900000)
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
         if (existingUserByEmail) {
-            true // TODO: will be back here
+            if (existingUserByEmail.isVerified) {
+                Response.json({
+                    success: false,
+                    message: "This email is already registered please try another one"
+                }, {status: 400})
+            }else{
+                existingUserByEmail.password = await bcrypt.hash(password, 10);
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
         } else {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const expiryDate = new Date();
-            expiryDate.setDate(expiryDate.getDate() + 1);
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setDate(expiryDate.getDate() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false, // Assuming default value
+                isAcceptingMessage: true, // Assuming default value
+                message: [] // Assuming default value
+            });
+
+            await newUser.save();
         }
 
-        new UserModel({
-            username,
-            email,
-            password: hashedPassword,
-            verifyCode,
-            verifyCodeExpiry: ex;
-            isVerified: boolean;
-            isAcceptingMessage: boolean;
-            message: Message[];
-        })
+        // verification email 
+        const emailResponse = await sendVeficationEmail(username, password, verifyCode);
+
+        if(!emailResponse.success) {
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            },{status: 500})
+        }
+
+        return Response.json({
+            success: true,
+            message: "Verification mail sent successfully"
+        },{status: 201})
+
     } catch (error) {
         console.error('error registering user', error)
         return Response.json(
             {
                 success: false,
                 message: "Error registering user"
-            }
+            }, {status: 500}
         )
     }
 }
